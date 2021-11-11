7171ef75b6f3a3a339691cca1b6ca872d4d94561
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-11 12:49:18 +0300

    update main.rs with data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let mut x = 45;
+    let mut x:u64 = 45;
+    let mut f:f32 = 6.7;
+    let mut b:bool =false;
     println!("the value of x is {}", x);
+    println!("the value of f is {}", f);
+    println!("the value of b is {}", b);
+
     x = 60;
+    f = 4.7;
+    b= true;
     println!("the value of x is {}", x);
+    println!("the value of f is {}", f);
+    println!("the value of b is {}", b);
 
 }
