23d396f1a7409e239196dc1b1b1e36525db40ddf
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-15 11:16:24 +0300

    update main.rs with else

--- if-statement/src/main.rs
+++ if-statement/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let n =20;
+    let n =45;
     if n < 30{
         println!("the number {} is less than 30",n);
+    }else {
+        println!("the number {} is greater than 30",n);
     }
 }
