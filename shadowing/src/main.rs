8f1ce1c620617d171f21db5eda74205972a02aef
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-22 14:06:32 +0300

    add main.rs file

---
+++ shadowing/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x  = 10;
+    {
+        let x =  15;
+        println!("x inside : {}", x);
+    }
+    println!("x inside : {}", x);
+
+    let x ="Xis a string";
+    println!("x is : {}", x);
+
+    let x =true;
+    println!("x is : {}", x);
+}
