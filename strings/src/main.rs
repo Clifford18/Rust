e7c8f8435f7a4c3d356b805807fade53923b711b
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-23 16:41:26 +0300

    add main.rs on strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_string = String::from ("How's it going? My name is CJ. ");
+    //length
+    println!("length {}",my_string.len());
+    //is empty
+    println!("String is empty {}",my_string.is_empty());
+
+    for token in my_string.split_whitespace(){
+        println!("{}",token);
+    }
+    println! ("Does the string contain 'CJ' {}",my_string.contains("CJ"));
+
+    my_string.push_str("Welcome to a tutorial on Strings!");
+
+    println!("{}",my_string);
+
+
+
 }
