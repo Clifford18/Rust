f9755d0ef892051b94623a18bac74ffbe13f08f0
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-29 15:34:59 +0300

    add main.rs to the files

--- exec-commands/src/main.rs
+++ exec-commands/src/main.rs
@@ -1,3 +1,19 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    //python cj.py
+    let mut cmd = Command::new("python");
+    cmd.arg("cj.py");
+
+    //execute the command
+    match cmd.output() {
+        Ok(o) => {
+            unsafe {
+                println!("Output : {}", String::from_utf8_unchecked(o.stdout));
+            }
+        },
+        Err(e) => {
+            println!("there was an error {}", e)
+        }
+    }
 }
