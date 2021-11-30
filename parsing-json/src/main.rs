a1953f9c2cd17e05d50fd5c4fbcaffd380cc3861
Clifford18 <cliffordjohn.cj@gmail.com>
2021-11-30 14:29:32 +0300

    update main.rs file

--- parsing-json/src/main.rs
+++ parsing-json/src/main.rs
@@ -1,18 +1,34 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
 
 use serde_json::Value as JsonValue;
 
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    is_male: bool,
+}
+
 fn main() {
-    let mut json_str = r#"
+    let json_str = r#"
+
     {
     "name":"CJ",
     "age":30,
-    "is_male":"true"
+    "is_male":true
     }
     "#;
     let res = serde_json::from_str(json_str);
     if res.is_ok() {
-        let p: JsonValue = res.unwrap();
-        println!("The name is {}", p["name"].as_str().unwrap());
-    } else { println!("Sorry could not parse JSON :(") }
+        // let p: JsonValue = res.unwrap();
+        let p: Person = res.unwrap();
+        println!("The name is {}", p.name);
+        println!("The age is {}", p.age);
+        println!("Are they male? {}", p.is_male);
+    } else {
+        println!("Sorry could not parse JSON :(")
+    }
 }
